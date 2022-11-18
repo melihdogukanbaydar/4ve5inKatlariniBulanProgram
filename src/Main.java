13d03ddcdc41fde5e4328cb00d0b61011a8001c8
Melih Doğukan Baydar <116119851+melihdogukanbaydar@users.noreply.github.com>
2022-11-18 22:11:50 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      int n;
+      Scanner sayi = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz : ");
+        n = sayi.nextInt();
+        for (int i =1; i <= n; i *=20){
+            System.out.println(i);
+        }
+    }
+}
